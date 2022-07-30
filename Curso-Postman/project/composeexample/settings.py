6375e9fd23faa83b9eeb06c0928ec7fc2857b0cb
briantecnology11 <109242484+briantecnology11@users.noreply.github.com>
2022-07-30 09:56:57 -0500

    Add files via upload

---
+++ Curso-Postman/project/composeexample/settings.py
@@ -0,0 +1,95 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = '12f+v*t*0u+r!!-+o*sq!z8=p9pbu6+gif_8kuvm%j!wj-_las'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_extensions',
+    'courses',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'composeexample.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ]
+        },
+    }
+]
+
+WSGI_APPLICATION = 'composeexample.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+    },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
---
+++ Curso-Postman/project/courses/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from courses.models import Course, Material, Teacher, Video, Comment
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ranking')
+
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('url',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Comment, CommentAdmin)
